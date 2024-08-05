0f52fad7d156aad518d92da19c97bcb1f4498725
jobordeau <74385376+jobordeau@users.noreply.github.com>
2024-08-05 02:37:23 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-    region = "eu-west-3c"  
+  region = "eu-west-3"
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-09d83d8d719da9808" # us-west-2
+  ami           = "ami-09d83d8d719da9808"
   instance_type = "t2.micro"
   tags = {
-      Name = "Devops"
+    Name = "Devops"
   }
 }
